b07ad0878e91dbed927b406f9687dda7a66091a7
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2024-01-16 16:09:17 -0500

    Update Sidebar.jsx

--- sidebar/Sidebar.jsx
+++ sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import {
   LinksArray,
   SecondarylinksArray,
-  SidebarCard,
   ToggleTema,
 } from "../../../index";
 import { v } from "../../../styles/variables";
