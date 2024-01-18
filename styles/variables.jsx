bfdeba155503c342c65afa7792deb7da29cb20b8
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2024-01-18 15:25:38 -0500

    Update variables.jsx

--- styles/variables.jsx
+++ styles/variables.jsx
@@ -12,7 +12,7 @@ import { TbBrandSupabase } from "react-icons/tb";
 import { FaReact } from "react-icons/fa";
 import { BsArrowDown, BsArrowUpShort } from "react-icons/bs";
 import { FaBalanceScale } from "react-icons/fa";
-import logo from "../assets/inventarioslogo.png";
+import logo from "../assets/ada369logo.png";
 import { FcGoogle } from "react-icons/fc";
 
 import { RiCloseLine } from "react-icons/ri";
