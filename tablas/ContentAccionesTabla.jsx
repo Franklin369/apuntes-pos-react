fb42f07bd66e887f6a23d98829fbbdc91e895f3a
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2024-02-01 18:28:11 -0500

    Update ContentAccionesTabla.jsx

--- tablas/ContentAccionesTabla.jsx
+++ tablas/ContentAccionesTabla.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { AccionTabla } from "../../../index";
 import { v } from "../../../styles/variables";
+import { Icon } from "@iconify/react";
 export function ContentAccionesTabla({funcionEditar, funcionEliminar}) {
   return (
     <Container>
       <AccionTabla funcion = {funcionEditar} fontSize="18px" color="#7d7d7d" icono={<v.iconeditarTabla/>} />
-      <AccionTabla funcion={funcionEliminar} fontSize="20px" color="#f76e8e" icono={<v.iconeliminarTabla/>} />
+      <AccionTabla funcion={funcionEliminar} fontSize="20px" color="#f76e8e" icono={<Icon icon="fluent-emoji-high-contrast:skull" />} />
     </Container>
   );
 }
